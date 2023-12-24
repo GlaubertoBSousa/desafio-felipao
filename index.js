4586dec324344f3cd385621205ae49c5e1ac6457
Glauberto Sousa <glauberto.b.sousa@gmail.com>
2023-12-24 20:11:16 -0300

    Desafio do Projeto Felipão DIO

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Teste");
+let xP = 1 
+
+for (let i = 0; i <10000;i++) {
+    xP +=1
+    if ( xP>0 && xP<=1000){
+        console.log("O herói é nível de Ferro")
+    } else if (xP>1001 && xP<=2000){
+        console.log("O herói é nível de Bronze")
+    }else if (xP>2001 && xP<=5000){
+        console.log("O herói é nível de Prata")
+    }else if (xP>5001 && xP<=7000){
+        console.log("O herói é nível de Ouro")
+    }else if (xP>7001 && xP<=8000){
+        console.log("O herói é nível de Platina")
+    }else if (xP>8001 && xP<=9000){
+        console.log("Agora o herói é nível de Ascendente")
+    }else if (xP>8001 && xP<=10000){
+        console.log("Parabéns herói é nível de Imortal")
+    }else  {
+        console.log("Parabéns herói é nível de Radiante")
+}
+}
